9d6c8111015f0d09828585465f92e85025055783
ken0063 <olukotunk@gmail.com>
2022-03-14 17:25:03 +0100

    ping app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebookF,
+  faInstagram,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
 
 function App() {
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState(false);
+
+  const submit = (e) => {
+    e.preventDefault();
+    setError(emailValidation(email));
+  };
+
+  const emailValidation = (email) => {
+    const regex =
+      // eslint-disable-next-line no-useless-escape
+      /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    if (!email || regex.test(email) === false) {
+      return "Please provide a valid email";
+    }
+    return;
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="logo">
+        <img src="../images/logo.svg" alt="logo" />
+      </div>
+      <div className="heading">
+        <h1>
+          We are launching <strong>soon!</strong>
+        </h1>
+        <p>Subscribe and get notified</p>
+      </div>
+      <div className="form">
+        <div>
+          <form className={!error ? "input-wrapper" : "input-wrapper error"}>
+            <input
+              type="text"
+              placeholder="Your email address..."
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </form>
+          <p className="error">{error}</p>
+        </div>
+
+        <button type="button" onClick={submit}>
+          Notify Me
+        </button>
+      </div>
+
+      <div className="bg-image">
+        <img src="../images/illustration-dashboard.png" alt="bg" />
+      </div>
+      <div className="socials">
+        <FontAwesomeIcon
+          icon={faFacebookF}
+          color="hsl(223, 87%, 63%)"
+          className="facebook"
+          size="sm"
+          border
+        />
+
+        <FontAwesomeIcon
+          icon={faTwitter}
+          color="hsl(223, 87%, 63%)"
+          className="twit"
+          size="sm"
+          border
+        />
+
+        <FontAwesomeIcon
+          icon={faInstagram}
+          color="hsl(223, 87%, 63%)"
+          className="insta"
+          size="sm"
+          border
+        />
+      </div>
+
+      <footer>© Copyright Ping. All rights reserved</footer>
     </div>
   );
 }
